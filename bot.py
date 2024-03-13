b6dc086be604e307e3de8e37972dbeae9038d9c4
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-03-14 00:12:38 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ class Bot(Ntbots):
         await super().start()
         me = await self.get_me()
         self.username = '@' + me.username
-        print(f"{me.first_name} with for Pyrogram v{self.version} (Layer {layer}) started on {me.username}.")
+        print(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
 
     async def stop(self, *args):
         await super().stop()
