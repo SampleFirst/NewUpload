1c5b366a31c061f46cfb228c33f838a77cd246c7
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-03-14 00:09:16 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,29 +1,43 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-import logging
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
-
 import os
+import logging
 from plugins.config import Config
-
 from pyrogram import Client as Ntbots
-from pyrogram import filters
+from pyrogram import filters, __version__
+from pyrogram.raw.all import layer
+
+# Configure logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
+class Bot(Ntbots):
+    def __init__(self):
+        super().__init__(
+            "UploadLinkToFileBot",
+            bot_token=Config.BOT_TOKEN,
+            api_id=Config.API_ID,
+            api_hash=Config.API_HASH,
+            plugins={"root": "plugins"},
+            workers=50,
+            sleep_threshold=5
+        )
 
-if __name__ == "__main__" :
-    # create download directory, if not exist
+    async def start(self):
+        await super().start()
+        me = await self.get_me()
+        self.username = '@' + me.username
+        print(f"{me.first_name} with for Pyrogram v{self.version} (Layer {layer}) started on {me.username}.")
+
+    async def stop(self, *args):
+        await super().stop()
+        print("Bot stopped. Bye.")
+
+
+if __name__ == "__main__":
+    # Create download directory if not exists
     if not os.path.isdir(Config.DOWNLOAD_LOCATION):
         os.makedirs(Config.DOWNLOAD_LOCATION)
-    plugins = dict(root="plugins")
-    Ntbots = Ntbots(
-        "UploadLinkToFileBot",
-        bot_token=Config.BOT_TOKEN,
-        api_id=Config.API_ID,
-        api_hash=Config.API_HASH,
-        plugins=plugins)
-    Ntbots.run()
 
+    # Start the bot
+    app = Bot()
+    app.run()
