407daf622f4befdc74b9cb8572bdb88f72b86ef8
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-03-14 12:44:35 +0530

    Update config.py

--- plugins/config.py
+++ plugins/config.py
@@ -1,13 +1,5 @@
 import os
 
-import logging
-
-logging.basicConfig(
-    format='%(name)s - %(levelname)s - %(message)s',
-    handlers=[logging.FileHandler('log.txt'),
-              logging.StreamHandler()],
-    level=logging.INFO
-)
 
 class Config(object):
     
