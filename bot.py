397d3bae2929acc16145124a4fd9cfde6eece5fa
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-03-14 23:15:07 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,25 +2,38 @@ import os
 import logging
 import logging.config
 
-from plugins.config import Config
-from pyrogram import Client as Ntbots
-from pyrogram import filters
-
 # Get logging configurations
 logging.config.fileConfig('logging.conf')
-logging.getLogger().setLevel(logging.WARNING)
+logging.getLogger().setLevel(logging.INFO)
+
+from pyrogram import Client, __version__
+from pyrogram.raw.all import layer
+from plugins.config import Config
+
+class Bot(Client):
+
+    def __init__(self):
+        super().__init__(
+            "UploadLinkToFileBot",
+            bot_token=Config.BOT_TOKEN,
+            api_id=Config.API_ID,
+            api_hash=Config.API_HASH,
+            workers=200,
+            plugins={"root": "plugins"},
+            sleep_threshold=10,
+        )
+        
+    async def start(self):
+        await super().start()
+        me = await self.get_me()
+        self.username = me.username
+        logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
+                                
+
+    async def stop(self, *args):
+        await super().stop()
+        logging.info("Bot Restarting.......")
 
 
-if __name__ == "__main__" :
-    # create download directory, if not exist
-    if not os.path.isdir(Config.DOWNLOAD_LOCATION):
-        os.makedirs(Config.DOWNLOAD_LOCATION)
-    plugins = dict(root="plugins")
-    Ntbots = Ntbots(
-        "UploadLinkToFileBot",
-        bot_token=Config.BOT_TOKEN,
-        api_id=Config.API_ID,
-        api_hash=Config.API_HASH,
-        plugins=plugins)
-    Ntbots.run()
-
+app = Bot()
+app.run()
