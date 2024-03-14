8eda1840c00b496363273b0015f10110ab59a171
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-03-14 23:39:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,9 +6,13 @@ import logging.config
 logging.config.fileConfig('logging.conf')
 logging.getLogger().setLevel(logging.INFO)
 
+from aiohttp import web
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
+from plugins import web_server
 from plugins.config import Config
+
+PORT = int(os.environ.get("PORT", "8080"))
 
 class Bot(Client):
 
@@ -18,9 +22,9 @@ class Bot(Client):
             bot_token=Config.BOT_TOKEN,
             api_id=Config.API_ID,
             api_hash=Config.API_HASH,
-            workers=200,
+            workers=150,
             plugins={"root": "plugins"},
-            sleep_threshold=10,
+            sleep_threshold=5,
         )
         
     async def start(self):
@@ -29,6 +33,10 @@ class Bot(Client):
         self.username = me.username
         logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
                                 
+        app = web.AppRunner(await web_server())
+        await app.setup()
+        bind_address = "0.0.0.0"
+        await web.TCPSite(app, bind_address, PORT).start()
 
     async def stop(self, *args):
         await super().stop()
