3dbc7fa0ece81ae3620bb79fe962debf90ad5fe5
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-03-14 20:22:36 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,42 +1,26 @@
 import os
 import logging
 import logging.config
+
 from plugins.config import Config
 from pyrogram import Client as Ntbots
-from pyrogram import filters, __version__
-from pyrogram.raw.all import layer
+from pyrogram import filters
 
 # Get logging configurations
 logging.config.fileConfig('logging.conf')
-logging.getLogger().setLevel(logging.INFO)
-logging.getLogger("pyrogram").setLevel(logging.ERROR)
-
-class Bot(Ntbots):
-    def __init__(self):
-        super().__init__(
-            "UploadLinkToFileBot",
-            bot_token=Config.BOT_TOKEN,
-            api_id=Config.API_ID,
-            api_hash=Config.API_HASH,
-            plugins={"root": "plugins"},
-            workers=50,
-            sleep_threshold=5
-        )
-
-    async def start(self):
-        # Create download directory if not exists
-        if not os.path.isdir(Config.DOWNLOAD_LOCATION):
-            os.makedirs(Config.DOWNLOAD_LOCATION)
-        await super().start()
-        me = await self.get_me()
-        self.username = '@' + me.username
-        print(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
-
-    async def stop(self, *args):
-        await super().stop()
-        print("Bot stopped. Bye.")
+logging.getLogger().setLevel(logging.WARNING)
 
 
-# Start the bot
-app = Bot()
-app.run()
+if __name__ == "__main__" :
+    # create download directory, if not exist
+    if not os.path.isdir(Config.DOWNLOAD_LOCATION):
+        os.makedirs(Config.DOWNLOAD_LOCATION)
+    plugins = dict(root="plugins")
+    Ntbots = Ntbots(
+        "UploadLinkToFileBot",
+        bot_token=Config.BOT_TOKEN,
+        api_id=Config.API_ID,
+        api_hash=Config.API_HASH,
+        plugins=plugins)
+    Ntbots.run()
+
