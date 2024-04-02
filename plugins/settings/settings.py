6c85cee0ef0946ec081e4cf9de2ec88fc07f6cfe
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-04-03 00:05:58 +0530

    Update settings.py

--- plugins/settings/settings.py
+++ plugins/settings/settings.py
@@ -1,5 +1,3 @@
-
-
 import asyncio
 from pyrogram import types, errors, enums
 from plugins.config import Config
@@ -9,21 +7,25 @@ async def OpenSettings(m: "types.Message"):
     usr_id = m.chat.id
     user_data = await db.get_user_data(usr_id)
     if not user_data:
-        await m.edit("Failed to fetch your data from database!")
+        await m.edit("Failed to fetch your data from the database!")
         return
+
     upload_as_doc = user_data.get("upload_as_doc", False)
     caption = user_data.get("caption", None)
     apply_caption = user_data.get("apply_caption", True)
     thumbnail = user_data.get("thumbnail", None)
+
     buttons_markup = [
         [types.InlineKeyboardButton(f"ᴜᴘʟᴏᴀᴅ ᴀs {'🎥 ᴠɪᴅᴇᴏ' if upload_as_doc else '🗃️ Fɪʟᴇ'}",
                                     callback_data="triggerUploadMode")],
         [types.InlineKeyboardButton(f"{'ᴄʜᴀɴɢᴇ' if thumbnail else '🌃 sᴇᴛ'} ᴛʜᴜᴍʙɴᴀɪʟ",
                                     callback_data="setThumbnail")]
     ]
+
     if thumbnail:
         buttons_markup.append([types.InlineKeyboardButton("🌆 sʜᴏᴡ ᴛʜᴜᴍʙɴᴀɪʟ",
                                                           callback_data="showThumbnail")])
+
     buttons_markup.append([types.InlineKeyboardButton("♨️ ᴄʟᴏsᴇ",
                                                       callback_data="close")])
 
@@ -34,11 +36,10 @@ async def OpenSettings(m: "types.Message"):
             disable_web_page_preview=True,
             parse_mode=enums.ParseMode.MARKDOWN
         )
-    except errors.MessageNotModified: pass
+    except errors.MessageNotModified:
+        pass
     except errors.FloodWait as e:
         await asyncio.sleep(e.x)
-        await show_settings(m)
+        await OpenSettings(m)
     except Exception as err:
         Config.LOGGER.getLogger(__name__).error(err)
-
-
