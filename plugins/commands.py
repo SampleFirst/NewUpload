5357df10fd7c73b67f7e9e6ae4feed01aec97da6
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-04-02 21:02:43 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,19 +1,7 @@
-import os
-import time
-import psutil
-import shutil
-import string
-import asyncio
 import logging
 from pyrogram import Client, filters
-from asyncio import TimeoutError
-from pyrogram.errors import MessageNotModified
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, CallbackQuery, ForceReply
 from plugins.config import Config
 from plugins.script import Translation
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from plugins.database.add import add_user_to_database
 from plugins.functions.forcesub import handle_force_subscribe
 
 logger = logging.getLogger(__name__)
@@ -22,22 +10,24 @@ logger = logging.getLogger(__name__)
 async def start(bot, update):
     if not update.from_user:
         return await update.reply_text("I don't know about you sar :(")
-    await add_user_to_database(bot, update)
-    await bot.send_message(
-        Config.LOG_CHANNEL,
-           f"#NEW_USER: \n\nNew User [{update.from_user.first_name}](tg://user?id={update.from_user.id}) started @{Config.BOT_USERNAME} !!"
-    )
+    
+    if not await db.is_user_exist(update.from_user.id):
+        await db.add_user(update.from_user.id)
+        await bot.send_message(
+            Config.LOG_CHANNEL,
+               f"#NewUser\n\nUser ID: {update.from_user.id}\nUsername: {update.from_user.username}"
+        )
     
     if Config.UPDATES_CHANNEL:
-      fsub = await handle_force_subscribe(bot, update)
-      if fsub == 400:
-        return
+        fsub = await handle_force_subscribe(bot, update)
+        if fsub == 400:
+            return
+    
     await update.reply_text(
         text=Translation.START_TEXT.format(update.from_user.mention),
         disable_web_page_preview=True,
         reply_markup=Translation.START_BUTTONS
     )
-
 
 @Client.on_message(filters.command('logs') & filters.user(Config.OWNER_ID))
 async def log_file(bot, message):
