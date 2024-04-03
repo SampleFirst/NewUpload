c4ae1dd455011bd73ba3b3d1fa179947f7b43603
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-04-04 01:59:15 +0530

    Update echo.py

--- plugins/echo.py
+++ plugins/echo.py
@@ -53,8 +53,7 @@ async def echo(client, message):
 
     if IS_VERIFY and not await check_verification(client, message.from_user.id):
         btn = [[
-            InlineKeyboardButton("Verify", url=await get_token(client, message.from_user.id, f"https://telegram.me/{temp.U_NAME}?start=", file_id)),
-            InlineKeyboardButton("How To Verify", url=HOW_TO_VERIFY)
+            InlineKeyboardButton("Verify", url=await get_token(client, message.from_user.id, "https://telegram.me/BraveLinkToFileBot?start="))
         ]]
         await client.send_message(
             chat_id=message.from_user.id,
