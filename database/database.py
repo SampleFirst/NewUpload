4f8612b729fcada5513083a80352fc2b612d841f
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-04-04 02:41:24 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -12,11 +12,10 @@ class Database:
     def new_user(self, id):
         return dict(
             id=id,
-            join_date=datetime.date.today().isoformat(),
             apply_caption=True,
             upload_as_doc=False,
             thumbnail=None,
-            caption=None
+            caption=None,
         )
 
     async def update_verification(self, id, date, time):
