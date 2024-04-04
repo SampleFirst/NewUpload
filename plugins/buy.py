c75c337ecfffa2ed725aaef9439e16874a1a624b
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-04-04 18:13:48 +0530

    Update buy.py

--- plugins/buy.py
+++ plugins/buy.py
@@ -2,13 +2,13 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 # Define the command handler
-@Client.on_message(filters.command("buy"))
-def buy_command_handler(client, message):
+@Client.on_message(filters.command("plan"))
+def plan(client, message):
     text = "Per Month: 99 Rupees\n"
     text += "All Allowed Links\n"
     text += "No Timeouts\n"
     text += "No Links Limit\n\n"
-    text += "Ask for QR code /upgrade\n"
+    text += "Ask for QR code /send \n"
     text += "Note: Payment Not Refundable"
     keyboard = InlineKeyboardMarkup(
         [
