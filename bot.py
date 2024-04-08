5b6f316fad89744b96e6eaf4923b6da8b3a3faaf
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-04-08 15:04:58 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,6 +9,7 @@ logging.getLogger().setLevel(logging.INFO)
 from aiohttp import web
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
+from datetime import date, datetime 
 from plugins import web_server
 from info import *
 
@@ -31,9 +32,15 @@ class Bot(Client):
             os.makedirs(DOWNLOAD_LOCATION)
         await super().start()
         me = await self.get_me()
+        U_NAME = me.username
         self.username = me.username
         logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
-                                
+        tz = pytz.timezone('Asia/Kolkata')
+        today = date.today()
+        now = datetime.now(tz)
+        time = now.strftime("%H:%M:%S %p")
+        await self.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(a=today, b=time, c=U_NAME))
+        
         app = web.AppRunner(await web_server())
         await app.setup()
         bind_address = "0.0.0.0"
