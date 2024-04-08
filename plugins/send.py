6bf49fc35e9e18a14b2e3a6eba20a9cb3237bd6f
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-04-08 20:20:49 +0530

    Update send.py

--- plugins/send.py
+++ plugins/send.py
@@ -1,34 +1,20 @@
-from pyrogram import Client, filters 
-from database.database import db
-from info import *
+from pyrogram import Client, filters
+from info import ADMINS 
 
-@Client.on_message(filters.command("send"))
-async def send_msg(bot, message):
+
+@Client.on_message(filters.private & filters.command("send"))
+async def send_to_admin(bot, message):
     if message.reply_to_message:
         if message.from_user.id not in ADMINS:
             await message.reply_to_message.forward(chat_id=ADMINS[0])
             await message.reply_text("<b>Your message has been successfully sent to Admin.</b>")
             return
         else:
-            await message.reply_text("<b>Use this command as a reply to any message to contact an admin. For example: /send</b>")
+            await message.reply_text("<b>You're an admin. You can't use this command.</b>")
     else:
-        target_id = message.text.split(" ", 1)[1]
-        out = "Users Saved In DB Are:\n\n"
-        success = False
-        try:
-            user = await bot.get_users(int(target_id))
-            users = await db.get_all_users()  # Assuming you have a db object
-            async for usr in users:
-                out += f"{usr['id']}\n"
-            if str(user.id) in out:
-                await message.copy(chat_id=user.id)
-                success = True
-            else:
-                success = False
-            if success:
-                await message.reply_text(f"<b>Your message has been successfully sent to {user.mention}.</b>")
-            else:
-                await message.reply_text("<b>This user hasn't started this bot yet!</b>")
-        except Exception as e:
-            await message.reply_text(f"<b>Error: {e}</b>")
+        await message.reply_text("<b>Reply to a message with /send to forward it to an admin.</b>")
 
+@Client.on_message(filters.private & ~filters.command("send") & filters.reply)
+async def forward_reply_to_user(bot, message):
+    if message.reply_to_message.forward_from.id in ADMINS:
+        await message.reply_to_message.forward(chat_id=message.from_user.id)
