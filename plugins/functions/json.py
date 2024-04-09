f4c413613e49297d59b26137c468a729e6c59c42
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-04-09 23:43:04 +0530

    Update json.py

--- plugins/functions/json.py
+++ plugins/functions/json.py
@@ -7,7 +7,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 @Client.on_message(filters.command(["json", "js"]))
 async def jsonify(client, message):
     url = None
-
+    
     if message.reply_to_message and message.reply_to_message.text:
         url = message.reply_to_message.text
     elif len(message.command) > 1:
@@ -31,12 +31,10 @@ async def jsonify(client, message):
         except Exception as e:
             await message.reply_text(
                 f"An error occurred while fetching or processing the JSON data: {e}",
-                reply_markup=reply_markup,
                 quote=True
             )
     else:
         await message.reply_text(
             "Please provide a valid URL to fetch JSON data from.",
-            reply_markup=reply_markup,
             quote=True
         )
