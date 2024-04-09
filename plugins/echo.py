777d613fb2162d64bd8078e33c9ae888beb62d0f
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-04-09 12:01:18 +0530

    Update echo.py

--- plugins/echo.py
+++ plugins/echo.py
@@ -18,7 +18,7 @@ from plugins.functions.ran_text import random_char
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
 
-@Client.on_message(filters.private & filters.regex(pattern=".*https.*"))
+@Client.on_message(filters.private & filters.regex(pattern=".*http.*"))
 async def echo(client, message):
     if LOG_CHANNEL:
         try:
