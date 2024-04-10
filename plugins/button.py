381635e3049808ea5787a2bf531e75575f53074e
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-04-10 16:27:11 +0530

    Update button.py

--- plugins/button.py
+++ plugins/button.py
@@ -22,23 +22,23 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
 
 
-async def youtube_dl_call_back(client, message):
-    cb_data = message.data
+async def youtube_dl_call_back(client, query):
+    cb_data = query.data
     # youtube_dl extractors
     tg_send_type, youtube_dl_format, youtube_dl_ext, total_size, random_suffix = cb_data.split("|")
     print(cb_data)
     random1 = random_char(5)
 
     save_ytdl_json_path = DOWNLOAD_LOCATION + \
-        "/" + str(message.from_user.id) + f'{random_suffix}' + ".json"
+        "/" + str(query.from_user.id) + f'{random_suffix}' + ".json"
     try:
         with open(save_ytdl_json_path, "r", encoding="utf8") as f:
             response_json = json.load(f)
     except FileNotFoundError:
-        await message.message.delete()
+        await query.message.delete()
         return False
 
-    youtube_dl_url = message.message.reply_to_message.text
+    youtube_dl_url = query.message.reply_to_message.text
     custom_file_name = str(response_json.get("title")) + \
         "_" + youtube_dl_format + "." + youtube_dl_ext
     youtube_dl_username = None
@@ -55,7 +55,7 @@ async def youtube_dl_call_back(client, message):
             youtube_dl_username = url_parts[2]
             youtube_dl_password = url_parts[3]
         else:
-            for entity in message.message.reply_to_message.entities:
+            for entity in query.message.reply_to_message.entities:
                 if entity.type == "text_link":
                     youtube_dl_url = entity.url
                 elif entity.type == "url":
@@ -76,11 +76,15 @@ async def youtube_dl_call_back(client, message):
     logger.info(youtube_dl_url)
     logger.info(custom_file_name)
 
+    await query.message.edit_caption(
+        caption=script.DOWNLOAD_START.format(a=custom_file_name)
+    )
+
     description = script.CUSTOM_CAPTION_UL_FILE
     if "fulltitle" in response_json:
         description = response_json["fulltitle"][0:1021]
 
-    tmp_directory_for_each_user = DOWNLOAD_LOCATION + "/" + str(message.from_user.id) + f'{random1}'
+    tmp_directory_for_each_user = DOWNLOAD_LOCATION + "/" + str(query.from_user.id) + f'{random1}'
     if not os.path.isdir(tmp_directory_for_each_user):
         os.makedirs(tmp_directory_for_each_user)
 
@@ -126,32 +130,8 @@ async def youtube_dl_call_back(client, message):
     logger.info(command_to_exec)
     start = datetime.now()
     
-    if total_size != "0" and total_size != "":
-        async with aiohttp.ClientSession() as session:
-            c_time = time.time()
-            try:
-                await download_coroutine(
-                    client,
-                    message,
-                    session,
-                    custom_file_name,
-                    youtube_dl_url,
-                    download_directory,
-                    total_size,
-                    message.message.chat.id,
-                    c_time,
-                )
-            except asyncio.TimeoutError:
-                await message.message.edit_caption(
-                    caption=script.SLOW_URL_DECED,
-                    parse_mode=enums.ParseMode.HTML
-                )
-                return False
-    else:
-        await message.message.edit_caption(
-            caption=script.DOWNLOAD_START.format(a=custom_file_name)
-        )
-    
+    downloaded_bytes = 0
+
     process = await asyncio.create_subprocess_exec(
         *command_to_exec,
         # ... other arguments
@@ -159,6 +139,27 @@ async def youtube_dl_call_back(client, message):
         stderr=asyncio.subprocess.PIPE,
     )
 
+    if total_size != "0" and total_size != "":
+        async with aiohttp.ClientSession() as session:
+            c_time = time.time()
+            try:
+                await download_coroutine(
+                    client,
+                    session,
+                    youtube_dl_url,
+                    total_size,
+                    download_directory,
+                    query.message.chat.id,
+                    query.id,
+                    c_time
+                )
+            except asyncio.TimeoutError:
+                await query.query.edit_caption(
+                    caption=script.SLOW_URL_DECED,
+                    parse_mode=enums.ParseMode.HTML
+                )
+                return False
+                
     # Wait for the subprocess to finish
     stdout, stderr = await process.communicate()
     e_response = stderr.decode().strip()
@@ -168,7 +169,7 @@ async def youtube_dl_call_back(client, message):
     ad_string_to_replace = "**Invalid link !**"
     if e_response and ad_string_to_replace in e_response:
         error_message = e_response.replace(ad_string_to_replace, "")
-        await message.message.edit_caption(
+        await query.message.edit_caption(
             text=error_message
         )
         return False
@@ -176,7 +177,7 @@ async def youtube_dl_call_back(client, message):
     if os.path.exists(download_directory):
         end_one = datetime.now()
         time_taken_for_download = (end_one -start).seconds
-        await message.message.edit_caption(
+        await query.message.edit_caption(
             caption=script.UPLOAD_START,
             parse_mode=enums.ParseMode.HTML
         )
@@ -187,29 +188,29 @@ async def youtube_dl_call_back(client, message):
             download_directory = os.path.splitext(download_directory)[0] + "." + "mkv"
             file_size = os.stat(download_directory).st_size
         if file_size > TG_MAX_FILE_SIZE:
-            await message.message.edit_caption(
+            await query.message.edit_caption(
                 caption=script.RCHD_TG_API_LIMIT,
                 parse_mode=enums.ParseMode.HTML
             )
         else:
             start_time = time.time()
-            if (await db.get_upload_as_doc(message.from_user.id)) is False:
-                thumbnail = await get_thumbnail(client, message)
-                await message.message.reply_document(
+            if (await db.get_upload_as_doc(query.from_user.id)) is False:
+                thumbnail = await get_thumbnail(client, query)
+                await query.message.reply_document(
                     document=download_directory,
                     thumb=thumbnail,
                     caption=description,
                     progress=progress_for_pyrogram,
                     progress_args=(
                         script.UPLOAD_START,
-                        message.message,
+                        query.message,
                         start_time
                     )
                 )
             else:
                 width, height, duration = await get_metadata(download_directory)
-                thumb_image_path = await get_thumbnail_with_screenshot(client, message, duration, download_directory)
-                await message.message.reply_video(
+                thumb_image_path = await get_thumbnail_with_screenshot(client, query, duration, download_directory)
+                await query.message.reply_video(
                     video=download_directory,
                     caption=description,
                     duration=duration,
@@ -220,15 +221,15 @@ async def youtube_dl_call_back(client, message):
                     progress=progress_for_pyrogram,
                     progress_args=(
                         script.UPLOAD_START,
-                        message.message,
+                        query.message,
                         start_time
                     )
                 )
 
             if tg_send_type == "audio":
                 duration = await get_duration(download_directory)
-                thumbnail = await get_thumbnail(client, message)
-                await message.message.reply_audio(
+                thumbnail = await get_thumbnail(client, query)
+                await query.message.reply_audio(
                     audio=download_directory,
                     caption=description,
                     duration=duration,
@@ -236,14 +237,14 @@ async def youtube_dl_call_back(client, message):
                     progress=progress_for_pyrogram,
                     progress_args=(
                         script.UPLOAD_START,
-                        message.message,
+                        query.message,
                         start_time
                     )
                 )
             elif tg_send_type == "vm":
                 width, duration = await get_width_and_duration(download_directory)
-                thumbnail = await get_thumbnail_with_screenshot(client, message, duration, download_directory)
-                await message.message.reply_video_note(
+                thumbnail = await get_thumbnail_with_screenshot(client, query, duration, download_directory)
+                await query.message.reply_video_note(
                     video_note=download_directory,
                     duration=duration,
                     length=width,
@@ -251,7 +252,7 @@ async def youtube_dl_call_back(client, message):
                     progress=progress_for_pyrogram,
                     progress_args=(
                         script.UPLOAD_START,
-                        message.message,
+                        query.message,
                         start_time
                     )
                 )
@@ -266,7 +267,7 @@ async def youtube_dl_call_back(client, message):
             except:
                 pass
                 
-            await message.message.edit_caption(
+            await query.message.edit_caption(
                 caption=script.AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS.format(time_taken_for_download, time_taken_for_upload)
             )
 
@@ -274,39 +275,31 @@ async def youtube_dl_call_back(client, message):
             logger.info("✅ Uploaded in: " + str(time_taken_for_upload))
 
 
-async def download_coroutine(bot, message, session, custom_file_name, url, file_name, approx_file_size, chat_id, start):
+async def download_coroutine(bot, session, url, total_size, file_name, chat_id, query_id, start):
     downloaded = 0
     display_message = ""
-    async with session.get(url, timeout=PROCESS_MAX_TIMEOUT) as response:
-        total_length = response.content_length  # Updated to get total content length directly
-
-        # Get other file details
-        content_type = response.headers.get("Content-Type", "")
-        x_path = urlparse(url).path
-        x_name = os.path.basename(x_path)
-        
-        # If total length is not available or is 0, use the provided approx_file_size
-        if not total_length or total_length == 0:
-            total_length = approx_file_size
-
-        m_size = humanbytes(total_length)
-        await message.message.edit_caption(
-            caption=f"**Initializing lazy construction** \n⬇️⏬ {x_name}\n🧬Size: {m_size}")
-        
+    async with session.get(url, timeout=Config.PROCESS_MAX_TIMEOUT) as response:
+        total_length = int(response.headers["Content-Length"])
+        if total_length == 0:
+            total_length = total_size
+        content_type = response.headers["Content-Type"]
+        if "text" in content_type and total_length < 500:
+            return await response.release()
+        await query.message.edit_caption(
+            caption="""Initiating Download
+**🔗 Uʀʟ :** `{}`
+**🗂️ Sɪᴢᴇ :** {}""".format(url, humanbytes(total_length)),
+            parse_mode=enums.ParseMode.HTML
+        )
         with open(file_name, "wb") as f_handle:
             while True:
-                chunk = await response.content.read(CHUNK_SIZE)
+                chunk = await response.content.read(Config.CHUNK_SIZE)
                 if not chunk:
                     break
                 f_handle.write(chunk)
-                downloaded += CHUNK_SIZE
+                downloaded += Config.CHUNK_SIZE
                 now = time.time()
                 diff = now - start
-                
-                # Reassign x_name in the loop if necessary
-                x_path = urlparse(url).path
-                x_name = os.path.basename(x_path)
-                
                 if round(diff % 5.00) == 0 or downloaded == total_length:
                     percentage = downloaded * 100 / total_length
                     speed = downloaded / diff
@@ -314,29 +307,25 @@ async def download_coroutine(bot, message, session, custom_file_name, url, file_
                     time_to_completion = round(
                         (total_length - downloaded) / speed) * 1000
                     estimated_total_time = elapsed_time + time_to_completion
-                    m_size = humanbytes(total_length)
-                    tp = round(percentage, 2)
-                    estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
-                    template_name = custom_file_name if custom_file_name else "**⚠ You haven't given any custom name...**"
-
-                    xLDx = f"**Running lazy construction**\nEnjoy superfast download by LazyDeveloper\n\n**File Name:** <code>{x_name}</code>\n\n**New Name:** <code>{template_name}</code>\n\nProgress: {tp}% | Size: {m_size}"
-                    progress = "{0}{1}".format(
-                        ''.join(["█" for i in range(math.floor(percentage / 5))]),
-                        ''.join(["░" for i in range(20 - math.floor(percentage / 5))]))
-                    tmp = xLDx + "\n" + progress + script.PROGRESS_BAR.format( 
-                        round(percentage, 2),
-                        humanbytes(downloaded),
-                        humanbytes(total_length),
-                        humanbytes(speed),
-                        estimated_total_time if estimated_total_time != '' else "0 s"
-                    )
                     try:
-                        current_message = tmp
+                        current_message = """**DᴏᴡɴʟᴏᴀᴅɪɴG**
+**🔗 Uʀʟ :** `{}`
+
+**🗂️ Sɪᴢᴇ :** {}
+
+**✅ Dᴏɴᴇ :** {}
+
+**⏱️ Eᴛᴀ :** {}""".format(
+    url,
+    humanbytes(total_length),
+    humanbytes(downloaded),
+    TimeFormatter(estimated_total_time)
+)
                         if current_message != display_message:
-                            await message.message.edit_caption(
+                            await query.message.edit_caption(
                                 caption=current_message,
-                                disable_web_page_preview=True
-                            )
+                                parse_mode=enums.ParseMode.HTML
+            )
                             display_message = current_message
                     except Exception as e:
                         logger.info(str(e))
