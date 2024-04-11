346d9b006e638a9ad44bcc4f58ddb26bdf09aae6
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-04-12 00:12:05 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,15 +1,14 @@
 import logging
 from pyrogram import Client, filters
-from info import *
-from Script import script
-from pyrogram import Client, filters, enums
-from pyrogram.errors import ChatAdminRequired, FloodWait
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram import enums
+from pyrogram.errors import ChatAdminRequired
 from database.database import db
 from utils import *
 
 logger = logging.getLogger(__name__)
 
-@Client.on_message(filters.command(["start"]) & filters.private)
+@Client.on_message(filters.command("start") & filters.private)
 async def start(client, message):
     if not await db.is_user_exist(message.from_user.id):
         await db.add_user(message.from_user.id)
@@ -22,46 +21,43 @@ async def start(client, message):
         try:
             invite_link = await client.create_chat_invite_link(int(AUTH_CHANNEL))
         except ChatAdminRequired:
-            logger.error("Mᴀᴋᴇ sᴜʀᴇ Bᴏᴛ ɪs ᴀᴅᴍɪɴ ɪɴ Fᴏʀᴄᴇsᴜʙ ᴄʜᴀɴɴᴇʟ")
+            logger.error("Make sure Bot is admin in Forcesub channel")
             return
         btn = [[
             InlineKeyboardButton("Update Channel", url=invite_link.invite_link)
         ]]
         await client.send_message(
             chat_id=message.from_user.id,
-            text="Please Join My Updates Channel to use this Bot!\n\nDue to Telegram Users Traffic, Only Channel Subscribers can use the Bot!\n\nNote: Once you join the update channel, do not leave to avoid being banned.",
+            text="Please Join My Updates Channel to use this Bot!\n\nDue to Telegram Users Traffic, Only Channel Subscribers can use the Bot!",
             reply_markup=InlineKeyboardMarkup(btn),
             parse_mode=enums.ParseMode.MARKDOWN
         )
         return
-        
+    
+    buttons = (
+        [
+            [
+                InlineKeyboardButton('⚙️ Settings', callback_data='opensettings')
+            ],
+            [
+                InlineKeyboardButton('❔ Help', callback_data='help'),
+                InlineKeyboardButton('👨‍🚒 About', callback_data='about')
+            ],
+            [
+                InlineKeyboardButton('⛔️ Close', callback_data='close')
+            ]
+        ]
+    )
+    
+    reply_markup = InlineKeyboardMarkup(buttons)
+            
     await message.reply_text(
         text=script.START_TEXT.format(message.from_user.mention),
         disable_web_page_preview=True,
-        reply_markup=script.START_BUTTONS
+        reply_markup=reply_markup
     )
     return
     
-    data = message.command[1]
-    if data.split("-", 1)[0] == "verify":
-        userid = data.split("-", 2)[1]
-        token = data.split("-", 3)[2]
-        if str(message.from_user.id) != str(userid):
-            return await message.reply_text(
-                text="<b>Iɴᴠᴀʟɪᴅ ʟɪɴᴋ ᴏʀ Exᴘɪʀᴇᴅ ʟɪɴᴋ !</b>"
-            )
-        is_valid = await check_token(client, userid, token)
-        if is_valid == True:
-            await message.reply_text(
-                text=f"<b>Hᴇʏ {message.from_user.mention}, Yᴏᴜ ᴀʀᴇ sᴜᴄᴄᴇssғᴜʟʟʏ ᴠᴇʀɪғɪᴇᴅ !\nNᴏᴡ ʏᴏᴜ ʜᴀᴠᴇ ᴜɴʟɪᴍɪᴛᴇᴅ ᴀᴄᴄᴇss ғᴏʀ ᴀʟʟ ᴍᴏᴠɪᴇs ᴛɪʟʟ ᴛʜᴇ ɴᴇxᴛ ᴠᴇʀɪғɪᴄᴀᴛɪᴏɴ ᴡʜɪᴄʜ ɪs ᴀғᴛᴇʀ 3 ʜᴏᴜʀs ғʀᴏᴍ ɴᴏᴡ.</b>"
-            )
-            await verify_user(client, userid, token)
-            return
-        else:
-            return await message.reply_text(
-                text="<b>Iɴᴠᴀʟɪᴅ ʟɪɴᴋ ᴏʀ Exᴘɪʀᴇᴅ ʟɪɴᴋ !</b>"
-            )
-
 @Client.on_message(filters.command('logs') & filters.user(ADMINS))
 async def log_file(bot, message):
     """Send log file"""
@@ -72,23 +68,49 @@ async def log_file(bot, message):
 
 @Client.on_message(filters.command('deletealldata') & filters.user(ADMINS))
 async def delete_all_data(bot, message):
+    buttons = (
+        [
+            [InlineKeyboardButton('Yes', callback_data="autodelete")],
+            [InlineKeyboardButton('Cancel', callback_data="close_data")]
+        ]
+    )
+    
+    reply_markup = InlineKeyboardMarkup(buttons)
+            
     await message.reply_text(
-        'Tʜɪs ᴡɪʟʟ ᴅᴇʟᴇᴛᴇ ᴀʟʟ.\nDᴏ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴄᴏɴᴛɪɴᴜᴇ ?',
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        text="Yᴇs", callback_data="autodelete"
-                    )
-                ],
-                [
-                    InlineKeyboardButton(
-                        text="Cᴀɴᴄᴇʟ", callback_data="close_data"
-                    )
-                ],
-            ]
-        ),
+        'This will delete all.\nDo you want to continue?',
+        reply_markup=reply_markup,
         quote=True,
     )
 
+@Client.on_message(filters.command('help') & filters.private)
+async def help(client, message):
+    buttons = (
+        [
+            [InlineKeyboardButton('⛔️ Close', callback_data='close')]
+        ]
+    )
+    
+    reply_markup = InlineKeyboardMarkup(buttons)
+    
+    await message.reply_text(
+        text=script.HELP_TEXT.format(message.from_user.mention),
+        disable_web_page_preview=True,
+        reply_markup=reply_markup
+    )
 
+@Client.on_message(filters.command('about') & filters.private)
+async def about(client, message):
+    buttons = (
+        [
+            [InlineKeyboardButton('⛔️ Close', callback_data='close')]
+        ]
+    )
+    
+    reply_markup = InlineKeyboardMarkup(buttons)
+    
+    await message.reply_text(
+        text=script.ABOUT_TEXT.format(message.from_user.mention),
+        disable_web_page_preview=True,
+        reply_markup=reply_markup
+    )
