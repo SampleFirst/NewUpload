5708432ee79ea02eb4f22751d5d12e2385bac141
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-04-12 00:27:35 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -2,7 +2,8 @@ import os
 import logging
 from pyrogram import Client, types
 from database.database import db
-from Script import script 
+from Script import script
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from plugins.settings.settings import opensettings
 from plugins.dl_button import ddl_call_back
 from plugins.button import youtube_dl_call_back
@@ -10,31 +11,59 @@ from plugins.button import youtube_dl_call_back
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
 
-
 @Client.on_callback_query()
 async def button(client, query):
     if query.data == "home":
+        buttons = [
+            [
+                InlineKeyboardButton('⚙️ Settings', callback_data='opensettings')
+            ],
+            [
+                InlineKeyboardButton('❔ Help', callback_data='help'),
+                InlineKeyboardButton('👨‍🚒 About', callback_data='about')
+            ],
+            [
+                InlineKeyboardButton('⛔️ Close', callback_data='close')
+            ]
+        ]
         await query.message.edit_text(
             text=script.START_TEXT.format(query.from_user.mention),
-            reply_markup=script.START_BUTTONS,
+            reply_markup=InlineKeyboardMarkup(buttons),
             disable_web_page_preview=True
         )
     elif query.data == "help":
+        buttons = [
+            [
+                InlineKeyboardButton('🏡 Home', callback_data='home'),
+                InlineKeyboardButton('👨‍🚒 About', callback_data='about')
+            ],
+            [
+                InlineKeyboardButton('⛔️ Close', callback_data='close')
+            ]
+        ]
         await query.message.edit_text(
-            text=script.HELP_TEXT,
-            reply_markup=script.HELP_BUTTONS,
+            text=script.HELP_TEXT.format(query.from_user.mention),
+            reply_markup=InlineKeyboardMarkup(buttons),
             disable_web_page_preview=True
         )
     elif query.data == "about":
+        buttons = [
+            [
+                InlineKeyboardButton('❔ Help', callback_data='help'),
+                InlineKeyboardButton('🏡 Home', callback_data='home')
+            ],
+            [
+                InlineKeyboardButton('⛔️ Close', callback_data='close')
+            ]
+        ]
         await query.message.edit_text(
-            text=script.ABOUT_TEXT,
-            reply_markup=script.ABOUT_BUTTONS,
+            text=script.ABOUT_TEXT.format(query.from_user.mention),
+            reply_markup=InlineKeyboardMarkup(buttons),
             disable_web_page_preview=True
         )
     elif query.data == "opensettings":
         await query.answer()
         await opensettings(query.message)
-     
     elif query.data == "showThumbnail":
         thumbnail = await db.get_thumbnail(query.from_user.id)
         if not thumbnail:
@@ -47,19 +76,24 @@ async def button(client, query):
                 reply_markup=types.InlineKeyboardMarkup(
                     [
                         [
-                            types.InlineKeyboardButton("Delete Thumbnail", callback_data="deleteThumbnail")
+                            types.InlineKeyboardButton("Delete Thumbnail", callback_data="deletethumbnail")
                         ]
                     ]
                 )
             )
-    elif query.data == "deleteThumbnail":
+    elif query.data == "deletethumbnail":
         await db.set_thumbnail(query.from_user.id, None)
         await query.answer("Okay, I deleted your custom thumbnail. Now I will apply default thumbnail.", show_alert=True)
-        await query.message.delete(True)
-    elif query.data == "setThumbnail":
+        await query.message.delete()
+    elif query.data == "setthumbnail":
+        buttons = [
+            [
+                InlineKeyboardButton('⛔️ Close', callback_data='close')
+            ]
+        ]
         await query.message.edit_text(
             text=script.TEXT,
-            reply_markup=script.BUTTONS,
+            reply_markup=InlineKeyboardMarkup(buttons),
             disable_web_page_preview=True
         )
     elif query.data == "triggerUploadMode":
@@ -72,7 +106,7 @@ async def button(client, query):
         await opensettings(query.message)
     elif query.data == "autodelete":
         await db.delete_all_data()
-        await query.message.edit_text('Sᴜᴄᴄᴇsғᴜʟʟʏ Dᴇʟᴇᴛᴇᴅ Aʟʟ Tʜᴇ Iɴᴅᴇxᴇᴅ Fɪʟᴇs.')
+        await query.message.edit_text('Successfully Deleted All The Users Data.')
     elif "close" in query.data:
         await query.message.delete()
     elif "|" in query.data:
@@ -81,4 +115,3 @@ async def button(client, query):
         await ddl_call_back(client, query)
     else:
         await query.message.delete()
-        
