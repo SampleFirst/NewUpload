06019f65cf38f7e2346a8d02743e56d93dccf993
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-04-13 16:43:30 +0530

    Update plans.py

--- plugins/plans.py
+++ plugins/plans.py
@@ -1,4 +1,4 @@
-from pyrogram import Client, filters
+from pyrogram import Client, filters, enums
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 # Define the command handler
@@ -6,7 +6,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 def plan(client, message):
     user_id = message.from_user.id
     name = message.from_user.mention 
-    text = f"User ID: '{user_id}'\nName: {name}\n\n💠 Premium\n\n✓ All Allowed URL\n✓ Task Limit: 1\n✓ Time Gap: NO\n✓ No Anti-Spam Timer\n✓ Validity: 1 MONTH\n\nAmount: 99 INR ₹\n\nBUY NOW FROM : @MyselfAstronaut"
+    text = f"User ID: '{user_id}'\nName: {name}\n\n💠 Premium\n\n✅ Zee5 - Non-DRM Links Support\n✅ Voot - Non-DRM Links Support\n✅ SonyLIV - Non-DRM Links Support\n✅ DiscoveryPlusIndia - Non-DRM Links Support\n✅ AnimalPlanet - Non-DRM Links Support\n✅ AmazonMiniTV - Non-DRM Links Support\n✅ Mxplayer - Non-DRM Links Support\n✅ YouTube - Links Support\n✅ TikTok - Links Support\n\n✅ Other yt-dlp Supported Sites.\n\n✓ Validity: 1 MONTH\n\nAmount: 99 INR ₹\n\nBUY NOW FROM : @MyselfAstronaut"
     keyboard = InlineKeyboardMarkup(
         [
             [
@@ -14,4 +14,11 @@ def plan(client, message):
             ]
         ]
     )
-    message.reply_text(text, reply_markup=keyboard)
+    message.reply_text(
+        text=text,
+        reply_markup=keyboard,
+        parse_mode=enums.ParseMode.MARKDOWN
+    )
+    
+    
+    
