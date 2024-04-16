484145aa64b4bfde29f2bdf3d6bc852c1ddc90ea
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-04-16 12:37:29 +0530

    Update echo.py

--- plugins/echo.py
+++ plugins/echo.py
@@ -53,17 +53,15 @@ async def echo(client, message):
 
     if IS_VERIFY and not await check_verification(client, message.from_user.id):
         btn = [[
-            InlineKeyboardButton("Verify", url=await get_token(client, message.from_user.id, f"https://telegram.me/BraveLinkToFileBot?start=")),
-            InlineKeyboardButton("How To Verify", url='https://t.me/BraveLinkToFileBot')
+            InlineKeyboardButton("Verify", url=await get_token(client, message.from_user.id, "https://telegram.me/BraveLinkToFileBot?start="))
         ]]
         await client.send_message(
             chat_id=message.from_user.id,
-            text="<b>You are not verified!\nKindly verify to continue!\nor\nUpgrade to our plan for use this bot\nUse /plans for Show Premium Plan Features.</b>",
-            disable_web_page_preview=True,
+            text="<b>You are not verified!\nKindly verify to continue so that you can get access to unlimited movies until  3 hours from now!</b>",
             parse_mode=enums.ParseMode.HTML,
             reply_markup=InlineKeyboardMarkup(btn)
         )
-        return 
+        return
         
     user_id = message.from_user.id
     act = temp.ACTIVE_URL.get(user_id)
