75fa0c8873cd9cd93ec03e7b8f58d025319b4f6c
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-04-17 19:14:09 +0530

    Update echo.py

--- plugins/echo.py
+++ plugins/echo.py
@@ -56,11 +56,10 @@ async def echo(client, message):
     act = temp.ACTIVE_URL.get(user_id)
     if act == True:
         msg_id = temp.STORE_ID.get(user_id)
-        msg = await client.get_messages(message.chat.id, msg_id)
         await client.send_message(
             chat_id=message.from_user.id,
             text="You are already processing a URL. Please wait until the current process finishes.",
-            reply_to_message_id=msg.id
+            reply_to_message_id=msg_id
         )
         return
     if IS_VERIFY and not await check_verification(client, message.from_user.id):
