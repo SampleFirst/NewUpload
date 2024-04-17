ee42fe5b135456741f539da5cfa565b3aca53adc
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-04-17 16:07:43 +0530

    Update token.py

--- plugins/token.py
+++ plugins/token.py
@@ -50,15 +50,16 @@ async def token_accepted(client, message, link):
         )
         return
     msg_id = temp.STORE_ID.get(user_id)
-    msg = await client.get_messages(message.chat.id, msg_id)
-    act = temp.ACTIVE_URL.get(user_id)
-    if act == True:
-        await client.send_message(
-            chat_id=message.from_user.id,
-            text="You are already processing a URL. Please wait until the current process finishes.",
-            reply_to_message_id=msg
-        )
-        return
+    if msg_id:
+        msg = await client.get_messages(message.chat.id, msg_id)
+        act = temp.ACTIVE_URL.get(user_id)
+        if act == True:
+            await client.send_message(
+                chat_id=message.from_user.id,
+                text="You are already processing a URL. Please wait until the current process finishes.",
+                reply_to_message_id=msg
+            )
+            return
     else:
         url = link
         youtube_dl_username = None
