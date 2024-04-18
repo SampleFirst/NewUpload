569138c73dff10eb662f56aeafde4052222f307f
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-04-18 22:55:04 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -118,7 +118,7 @@ async def get_token_short(bot, userid, link):
     date_var = short["date"]
     time_var = short["time"]
     
-    if int(short_var) == 5:
+    if int(short_var) >= 5:
         vr_num = 1
         short_verify_url = await get_verify_short_link(vr_num, url)
     else:
