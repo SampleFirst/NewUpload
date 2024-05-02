3b56ffa37a4751568d88c2a3231860a4d12794e0
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-05-02 15:45:25 +0530

    Update add.py

--- plugins/add.py
+++ plugins/add.py
@@ -1,26 +1,25 @@
 from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from utils import verify_user
 from info import PREMIUM_CHAT
 
-@Client.on_message(filters.command("add24") & filters.chat("PREMIUM_CHAT"))
+@Client.on_message(filters.command("add24") & filters.chat(PREMIUM_CHAT))
 async def add24(client, message):
     if len(message.command) != 2:
-        await message.reply_text("Invalid command format. Please use /add24 {userid} | {token}")
+        await message.reply_text("Invalid command format. Please use /add24 {userid}|{token}")
         return
     
-    user_info = message.text.split("|")
-    userid = user_info[0].split(" ")[1].strip()
+    user_info = message.command[1].split("|")
+    userid = user_info[0].strip()
     token = user_info[1].strip()
     
     try:
         await verify_user(client, userid, token)
         await client.send_message(
-            userid,
-            "For User: You are successfully verified for the next 24 hours.",
+            int(userid),
+            "You are successfully verified for the next 24 hours.",
             reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Click here", url="https://t.me/BraveUpdates/6")]])
         )
-        await message.reply(f"{userid} successfully verified for the next 24 hours for all 4 bots.\n\nFor user: Your verification is successful for the next 24 hours for all my bots!")
+        await message.reply_text(f"{userid} successfully verified for the next 24 hours for all 4 bots.\n\nYour verification is successful for the next 24 hours for all my bots!")
     except Exception as e:
         await message.reply_text(f"An error occurred: {str(e)}")
-        
-        
