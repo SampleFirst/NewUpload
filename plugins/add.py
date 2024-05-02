5fba813bcc43f052f6f091243dff6f0fcf954a9c
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-05-02 20:20:59 +0530

    Update add.py

--- plugins/add.py
+++ plugins/add.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from utils import verify_user
+from utils import check_token, verify_user
 from info import ADMINS
 
 
@@ -14,13 +14,18 @@ async def addpre(client, message):
         user_token = message.command[1].split('-')
         userid = int(user_token[0])
         token = user_token[1]
-        
-        await verify_user(client, userid, token)
-        await client.send_message(
-            userid,
-            "For User: You are successfully verified for the next 24 hours.",
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Click here", url="https://t.me/BraveUpdates/6")]])
-        )
-        await message.reply(f"{userid} successfully verified for the next 24 hours for all 4 bots.\n\nFor user: Your verification is successful for the next 24 hours for all my bots!")
+        is_valid = await check_token(client, userid, token)
+        if is_valid == True:
+            await verify_user(client, userid, token)
+            await client.send_message(
+                userid,
+                "For User: You are successfully verified for the next 24 hours.",
+                reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Click here", url="https://t.me/BraveUpdates/6")]])
+            )
+            await message.reply(f"{userid} successfully verified for the next 24 hours for all 4 bots.\n\nFor user: Your verification is successful for the next 24 hours for all my bots!")
+        else:
+            return await message.reply_text(
+                text="<b>Invalid link or expired link!</b>"
+            )
     except Exception as e:
         await message.reply_text(f"An error occurred: {str(e)}")
