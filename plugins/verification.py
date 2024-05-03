8bcc9d999e7edc8474abdec4ad51aa70c2ce344b
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-05-04 02:26:49 +0530

    Update verification.py

--- plugins/verification.py
+++ plugins/verification.py
@@ -4,10 +4,9 @@ from info import ADMINS
 
 @Client.on_message(filters.command("update_users") & filters.user(ADMINS))
 async def update_users(client, message):
-    async for user in db.get_all_users():
+    async for user in await db.get_all_users():  # Added await here
         user_id = user.get("id")
         default_verification_status = await db.get_verified(user_id)
         await db.update_verification(user_id, **default_verification_status)
     
     await message.reply_text("All users updated with default verification status.")
- 
