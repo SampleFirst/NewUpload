dc8f161f111e3043e5502f3f5b9d6eb8ec72419a
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-05-03 23:49:07 +0530

    Create verification.py

---
+++ plugins/verification.py
@@ -0,0 +1,13 @@
+from pyrogram import Client, filters
+from database.database import db
+from info import ADMINS 
+
+@Client.on_message(filters.command("update_users") & filters.user(ADMINS))
+async def update_users(client, message):
+    async for user in db.get_all_users():
+        user_id = user.get("id")
+        default_verification_status = await db.get_verified(user_id)
+        await db.update_verification(user_id, **default_verification_status)
+    
+    await message.reply_text("All users updated with default verification status.")
+ 
