d539bc97b7fd601fe2d19ccaf12bf0ea51524ca2
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-05-22 17:34:42 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -17,14 +17,12 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 TOKENS = {}
-SPL_TOKENS = {}
 VERIFIED = {}
 BANNED = {}
 
 # temp db for banned 
 class temp(object):
     VERIFY = {}
-    VERIFY_SHORT = {}
     ACTIVE_URL = {}
     TOKEN_ACCEPTED = {}
     STORE_ID = {}
@@ -92,14 +90,6 @@ async def remove_premium_user(bot, userid):
     date_var, time_var = str(date_var).split(" ")
     await remove_premium_status(bot, user.id, short_var, date_var, temp_time)
 
-async def send_special_verify_log(bot, userid, short, date, time):
-    user = await bot.get_users(int(userid))
-    if short == 1:
-        shortnum = 4
-    else:
-        shortnum = short - 1
-    log_message = f"#SpecialLog:\nUser ID: {user.id}\nUser Name: {user.mention}\nShortNum: {shortnum}\nDate: {date}\nTime: {time}"
-    await bot.send_message(LOG_CHANNEL, log_message)
 
 async def send_verify_log(bot, userid, short, date, time):
     user = await bot.get_users(int(userid))
@@ -110,14 +100,6 @@ async def send_verify_log(bot, userid, short, date, time):
     log_message = f"#VerificationLog:\nUser ID: {user.id}\nUser Name: {user.mention}\nShortNum: {shortnum}\nDate: {date}\nTime: {time}"
     await bot.send_message(LOG_CHANNEL, log_message)
 
-async def update_special_verify_status(bot, userid, short_temp, date_temp, time_temp):
-    short = await get_verify_status(userid)
-    short["short"] = short_temp
-    short["date"] = date_temp
-    short["time"] = time_temp
-    temp.VERIFY_SHORT[userid] = short
-    await db.update_verification(userid, short_temp, date_temp, time_temp)
-    await send_special_verify_log(bot, userid, short_temp, date_temp, time_temp)
 
 async def update_verify_status(bot, userid, short_temp, date_temp, time_temp):
     status = await get_verify_status(userid)
@@ -127,32 +109,14 @@ async def update_verify_status(bot, userid, short_temp, date_temp, time_temp):
     temp.VERIFY[userid] = status
     await db.update_verification(userid, short_temp, date_temp, time_temp)
     await send_verify_log(bot, userid, short_temp, date_temp, time_temp)
-    
-async def verify_special_user(bot, userid, token): #verify_special_frist_short_user
-    user = await bot.get_users(int(userid))
-    SPL_TOKENS[user.id] = {token: True}
-    tz = pytz.timezone('Asia/Kolkata')
-    short = await get_verify_status(user.id)
-    short_var = short["short"]
-    shortnum = int(short_var)
-    if shortnum == 4:
-        vrnum = 1
-        date_var = datetime.now(tz)+timedelta(hours=24)
-        temp_time = date_var.strftime("%H:%M:%S")
-        date_var, time_var = str(date_var).split(" ")
-    else:
-        vrnum = shortnum + 1
-        date_var = datetime.now(tz)
-        temp_time = date_var.strftime("%H:%M:%S")
-        date_var, time_var = str(date_var).split(" ")
-    await update_special_verify_status(bot, user.id, vrnum, date_var, temp_time)
 
-async def verify_user(bot, userid, token): #verify_short_user
+
+async def verify_user(bot, userid, token): 
     user = await bot.get_users(int(userid))
     TOKENS[user.id] = {token: True}
     short = await get_verify_status(user.id)
     tz = pytz.timezone('Asia/Kolkata')
-    date_var = datetime.now(tz)
+    date_var = datetime.now(tz)-timedelta(hours=25)
     temp_time = date_var.strftime("%H:%M:%S")
     date_var, time_var = str(date_var).split(" ")
     short_var = short["short"]
@@ -163,19 +127,7 @@ async def verify_user(bot, userid, token): #verify_short_user
         vrnum = shortnum + 1
     await update_verify_status(bot, user.id, vrnum, date_var, temp_time)
 
-async def check_special_token(bot, userid, token):
-    user = await bot.get_users(userid)
-    if user.id in SPL_TOKENS.keys():
-        STKN = SPL_TOKENS[user.id]
-        if token in STKN.keys():
-            is_used = STKN[token]
-            if is_used == True:
-                return False
-            else:
-                return True
-    else:
-        return False
-        
+
 async def check_token(bot, userid, token):
     user = await bot.get_users(userid)
     if user.id in TOKENS.keys():
@@ -188,7 +140,8 @@ async def check_token(bot, userid, token):
                 return True
     else:
         return False
-        
+
+
 async def get_verify_short_link(num, link):
     if int(num) == 1:
         API = VERIFY1_API
@@ -250,24 +203,8 @@ async def get_verify_short_link(num, link):
             else:
                 return f'https://{URL}/api?api={API}&link={link}'
 
-async def get_special_token(bot, userid, link): #get_token_special_short
-    user = await bot.get_users(userid)
-    token = ''.join(random.choices(string.ascii_letters + string.digits, k=7))
-    SPL_TOKENS[user.id] = {token: False}
-    url = f"{link}sverify-{user.id}-{token}"
-    await bot.send_message(LOG_CHANNEL, url)
-    short = await get_verify_status(user.id)
-    short_var = short["short"]
-    short_num = int(short_var)
-    if short_num == 4:
-        vr_num = 1
-        short_verify_url = await get_verify_short_link(vr_num, url)
-    else:
-        vr_num = short_num + 1
-        short_verify_url = await get_verify_short_link(vr_num, url)
-    return str(short_verify_url)
-    
-async def get_token(bot, userid, link): #get_token_short
+
+async def get_token(bot, userid, link): 
     user = await bot.get_users(userid)
     token = ''.join(random.choices(string.ascii_letters + string.digits, k=7))
     TOKENS[user.id] = {token: False}
@@ -276,21 +213,24 @@ async def get_token(bot, userid, link): #get_token_short
     short = await get_verify_status(user.id)
     short_var = short["short"]
     short_num = int(short_var)
-    if short_num == 4:
+    if short_num > 4:
         vr_num = 1
-        short_verify_url = await get_verify_short_link(vr_num, url)
-    else:
+    elif short_num < 4:
         vr_num = short_num + 1
-        short_verify_url = await get_verify_short_link(vr_num, url)
+    else:
+        vr_num = 1
+    short_verify_url = await get_verify_short_link(vr_num, url)
     return str(short_verify_url)
-    
+
+        
 async def get_verify_status(userid):
     status = temp.VERIFY.get(userid)
     if not status:
         status = await db.get_verified(userid)
         temp.VERIFY[userid] = status
     return status
-    
+
+
 async def check_verification(bot, userid):
     user = await bot.get_users(int(userid))
     tz = pytz.timezone('Asia/Kolkata')
